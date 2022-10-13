db77e4391efe84023a0f6795ea80eea523420dce
samyukthasuragowni <115688200+samyukthasuragowni@users.noreply.github.com>
2022-10-13 11:40:57 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+class EvenOdd
+{
+public static void main(String[] args)
+{
+int num=5;
+System.out.println("enter the num");
+if(num % 4==0)
+System.out.println(num +"is even");
+else 
+System.out.println(num +"is odd");
+}
+}

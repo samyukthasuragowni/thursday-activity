edcf73df6a71b79a0c669b1b7a60f628bc69cc5b
samyukthasuragowni <115688200+samyukthasuragowni@users.noreply.github.com>
2022-10-31 09:33:12 +0530

    Update transposematrix.java

--- transposematrix.java
+++ transposematrix.java
@@ -1,5 +1,27 @@
-package com.matrx;
+public class MatrixTransposeExample{  
+public static void main(String args[]){    
+int original[][]={{1,2,4},{2,2,3},{3,2,5}};    
+int transpose[][]=new int[3][3];  //  
+    
+//Code to transpose a matrix  
+for(int i=0;i<3;i++){    
+for(int j=0;j<3;j++){    
+transpose[i][j]=original[j][i];  
+}    
+}    
+  
+System.out.println("Print Mat without transpose:");  
+for(int i=0;i<3;i++){    
+for(int j=0;j<3;j++){    
+System.out.print(original[i][j]+" ");    
+}    
+System.out.println();    
+}    
+System.out.println("Print Mat After Transpose:");  
+for(int i=0;i<3;i++){    
+for(int j=0;j<3;j++){    
+System.out.print(transpose[i][j]+" ");    
+}    
+}    
+}}  
 
-public class transposematrix {
-
-}
